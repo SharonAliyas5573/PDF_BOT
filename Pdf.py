1c2faeb17f08453cdadf62d3af9807f30f7e66c8
SharonAliyas5573 <116248803+SharonAliyas5573@users.noreply.github.com>
2023-01-07 10:37:52 +0530

    add proper comment

--- Pdf.py
+++ Pdf.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -7,80 +8,150 @@ from selenium.webdriver.support import expected_conditions as EC
 import lxml
 import time
 import re
-PATH="C:\Program Files (x86)\chromedrver.exe"
+PATH = "C:\Program Files (x86)\chromedrver.exe"
 
-key=input("Enter Pdf Name:")
 
+
+
+# Get the file name from the user
+key = input("Enter Pdf Name:")
+
+# Replace spaces with '+' in the file name
 key = re.sub('\s+', '+', key)
 
-website_url="https://www.pdfdrive.com/search?q="+key+"&pagecount=&pubyear=&searchin=&em="
+# Set the base URL of the website
+web_link="https://www.pdfdrive.com"
 
+# Set the search URL
+website_url = "https://www.pdfdrive.com/search?q=" +key+"&pagecount=&pubyear=&searchin=&em="
 
-response =requests.get(website_url)
-soup = BeautifulSoup(response.text,'lxml')
+#  Send a request to the search URL
+response = requests.get(website_url)
 
-Head =soup.find_all("h2")
-for i in range(0,len(Head)):
-    Head [i]= Head[i].get_text()
+# Parse the HTML content
+soup = BeautifulSoup(response.text, 'lxml')
 
+# Find all the book titles
+Head = soup.find_all("h2")
 
-    
-info =soup.find_all("div", {"class": "file-info"})
+# Extract the text from the titles
+for i in range(0, len(Head)):
+    Head[i] = Head[i].get_text()
 
-for i in range(0,len(info)):
-    info [i]= info[i].get_text()
+# Find all the file info elements
+info = soup.find_all("div", {"class": "file-info"})
 
-info =  ['-'.join(x.replace('\n', '').split('·')[:-1]) for x in info]
+# Extract the text from the file info elements
+for i in range(0, len(info)):
+    info[i] = info[i].get_text()
 
+# Clean up the file info text
+info = ['-'.join(x.replace('\n', '').split('·')[:-1]) for x in info]
 
- 
+#Create a dictionary to store the book titles and file info
 data = {}
 for key, value in zip(Head, info):
     data[key] = value
 
+# Print the book titles and file info
 for index, (key, value) in enumerate(data.items()):
     print(f'{index+1}. {key}: {value}\n')
 
+# Find all the download links
+links = soup.find_all("a", {"class": "ai-search"})
 
-links =soup.find_all("a",{"class":"ai-search"})
-link=[]
+# Extract the download links
+link = []
 for i in links:
     link.append(i['href'])
-    
-INDEX_NO=int(input("Select BOOK wanted :"))
-INDEX_NO = (INDEX_NO-1)
 
-dwnld_link=link[INDEX_NO]
+# Get the index of the book that the user wants to download
+INDEX_NO = int(input("Select BOOK wanted :"))
+INDEX_NO =(INDEX_NO-1)
 
-dwnld_link="https://www.pdfdrive.com"+dwnld_link
+# Get the download link for the selected book
+dwnld_link = link[INDEX_NO]
 
-response_1=requests.get(dwnld_link)
+# Construct the full download link
+dwnld_link = web_link+dwnld_link
 
+# Send a request to the download page
+response_1 = requests.get(dwnld_link)
 
-soup1 = BeautifulSoup(response_1.text,'lxml')
+# Parse the HTML content
+soup1 = BeautifulSoup(response_1.text, 'lxml')
 
-dwnld_link_2=soup1.find_all("a",{"id":"download-button-link"})
+# Find the download link
+dwnld_link_2 = soup1.find_all("a", {"id": "download-button-link"})
 
+# Extract the download link
 for i in dwnld_link_2:
-    dwnld_link_2=i['href']
+    dwnld_link_2 = i['href']
 
-dwnld_link_2="https://www.pdfdrive.com"+dwnld_link_2
+# Construct the full download link
+dwnld_link_2 = web_link+dwnld_link_2
 
-driver = webdriver.Chrome()
+# Set up the Chrome driver options
+options = webdriver.ChromeOptions()
+options.add_argument('--ignore-certificate-errors')
+
+# Create a Chrome driver instance
+driver = webdriver.Chrome(chrome_options=options)
+
+# Navigate to the download page
 driver.get(dwnld_link_2)
 
+# Wait for the page to load
+driver.implicitly_wait(15)
 
-button = driver.find_element(By.CLASS_NAME, 'btn-user')
+# Get the HTML source of the page
+html = driver.page_source
 
-button.click()
-# html_source = driver.page_source
+# Close the driver
+driver.close()
 
-# soup2= BeautifulSoup(html_source, 'lxml')
+# Parse the HTML content
+soup2 = BeautifulSoup(html, 'lxml')
 
-# dwnld_link_3=soup2.find_all("a",{"id":"btn btn-primary btn-user"})
+# Find the download button
+divs = soup2.find_all("div", {"class": "text-center"})
 
-# for i in dwnld_link_3:
-#     dwnld_link_3=i['href']
+# Check if the book is available for download
+href = None
+if divs == None:
+    print("\n BOOK NOT AVAILABLE !")
+    exit()
+else:
+    for div in divs:
+        links = div.find_all('a', {'class':'btn btn-primary btn-user'})
+        for link in links:
+            href = link['href']
+            
+# Construct the full download link
+    final_link =web_link+href
+# Set the downloads folder
+    downloads_folder = os.path.join(os.path.expanduser('~'), 'Downloads')
+# Set the file name
+    FILE_NAME=Head[INDEX_NO]
+    words = FILE_NAME.split()
+    FILE_NAME = words[:5]
+    FILE_NAME = " ".join(FILE_NAME)
 
+# Set the file path
+    file_path = os.path.join(downloads_folder,(FILE_NAME+".pdf"))
+# Send a request to download the file
+    response = requests.get(final_link)
 
+# Check if the  file is available
+    if response.status_code == 200:
+
+    # Save the file
+        with open(file_path, "wb") as f:
+        # Write the data to the file
+            f.write(response.content)
+   
+
+        print("SUCCESSFULLY DOWNLOADED")
+    else:
+        print("BOOK NOT AVAILABLE")
 
