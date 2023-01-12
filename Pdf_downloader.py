bc9b1ca3ac0351fa7987f53f6271a7d250b7cc3e
Sharon <116248803+SharonAliyas5573@users.noreply.github.com>
2023-01-12 09:39:44 +0530

    Update Pdf_downloader.py

--- Pdf_downloader.py
+++ Pdf_downloader.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import os
+import urllib.request
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -11,9 +12,6 @@ import time
 import re
 
 
-os.environ['webdriver.chrome.driver'] = "C:\Program Files (x86)\chromedrver.exe"
-
-
 # Get the file name from the user
 key = input("Enter Pdf Name:")
 
@@ -21,10 +19,11 @@ key = input("Enter Pdf Name:")
 key = re.sub('\s+', '+', key)
 
 # Set the base URL of the website
-web_link="https://www.pdfdrive.com"
+web_link = "https://www.pdfdrive.com"
 
 # Set the search URL
-website_url = "https://www.pdfdrive.com/search?q=" +key+"&pagecount=&pubyear=&searchin=&em="
+website_url = "https://www.pdfdrive.com/search?q=" + \
+    key+"&pagecount=&pubyear=&searchin=&em="
 
 #  Send a request to the search URL
 response = requests.get(website_url)
@@ -49,7 +48,7 @@ for i in range(0, len(info)):
 # Clean up the file info text
 info = ['-'.join(x.replace('\n', '').split('·')[:-1]) for x in info]
 
-#Create a dictionary to store the book titles and file info
+# Create a dictionary to store the book titles and file info
 data = {}
 for key, value in zip(Head, info):
     data[key] = value
@@ -65,10 +64,10 @@ links = soup.find_all("a", {"class": "ai-search"})
 link = []
 for i in links:
     link.append(i['href'])
-  
+
 # Get the index of the book that the user wants to download
 INDEX_NO = int(input("Select BOOK wanted :"))
-INDEX_NO =(INDEX_NO-1)
+INDEX_NO = (INDEX_NO-1)
 
 # Get the download link for the selected book
 dwnld_link = link[INDEX_NO]
@@ -92,12 +91,13 @@ for i in dwnld_link_2:
 # Construct the full download link
 dwnld_link_2 = web_link+dwnld_link_2
 
-# Set up the Chrome driver options
-options = webdriver.ChromeOptions()
-options.add_argument('--ignore-certificate-errors')
+chrome_options = webdriver.ChromeOptions()
+prefs = {"profile.default_content_setting_values.notifications": 1}
+chrome_options.add_experimental_option("prefs", prefs)
+chrome_options.add_argument('--ignore-certificate-errors')
+chrome_options.add_argument('--headless')
+driver = webdriver.Chrome(chrome_options=chrome_options)
 
-# Create a Chrome driver instance
-driver = webdriver.Chrome(chrome_options=options)
 
 # Navigate to the download page
 driver.get(dwnld_link_2)
@@ -124,37 +124,25 @@ if divs == None:
     exit()
 else:
     for div in divs:
-        links = div.find_all('a', {'class':'btn btn-primary btn-user'})
+        links = div.find_all('a')
         for link in links:
             href = link['href']
-            
+    if href == "":
+        print("BOOK NOT AVAILABELE")
+        exit()
 # Construct the full download link
-    final_link =web_link+href
-# Set the downloads folder
-    downloads_folder = os.path.join(os.path.expanduser('~'), 'Downloads')
-# Set the file name
-    FILE_NAME=Head[INDEX_NO]
-    words = FILE_NAME.split()
-    FILE_NAME = words[:5]
-    FILE_NAME = " ".join(FILE_NAME)
-
-# Set the file path
-    file_path = os.path.join(downloads_folder,(FILE_NAME+".pdf"))
-# Send a request to download the file
-    response = requests.get(final_link)
-
-# Check if the  file is available
-    if href !="":
-        if response.content:
-
-    # Save the file
-            with open(file_path, "wb") as f:
-        # Write the data to the file
-                f.write(response.content)
-   
-
-            print("SUCCESSFULLY DOWNLOADED")
-        else:
-            print("BOOK NOT AVAILABLE")
+    if href.startswith('\/'):
+        final_link = web_link+href
     else:
-            print("BOOK NOT AVAILABLE")
+        final_link = href
+    
+    file_name = input("Enter the name for the downloaded file: [press Enter to set Defualt] ")
+    if file_name=="":
+        file_name="Ebook.pdf"
+    else:
+        file_name=file_name+'.pdf'
+# set the download path to defualt downloads  folder
+    file_path = os.path.join(os.path.expanduser("~"), "Downloads", file_name)
+# Download the file
+    urllib.request.urlretrieve(final_link, file_path)
+    print(f"File downloaded and saved as {file_path}")
